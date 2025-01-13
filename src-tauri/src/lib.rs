d6dfa4f0f0d65d1f2f8117824c0088ce5d6a7c2a
AHQ Miness <ahqsecret@gmail.com>
2025-01-13 20:47:06 +0530

    Do not use updater on android

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,7 +17,6 @@ pub fn run() {
 
             Ok(())
         })
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
