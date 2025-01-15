e4dc2493311d34d5cab4a6bae1de5797b5d95126
AHQ Miness <ahqsecret@gmail.com>
2025-01-15 14:01:03 +0530

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <>
       <WindowTitlebar data-tauri-drag-region>
+        <img src="/icon.png" className="ml-2 my-2 w-[26px] h-[26px]" />
+        <h1 className="my-auto ml-2 text-md font-sans">AHQ Store</h1>
+        <h1 className="my-auto ml-1 text-neutral-content text-md font-sans italic font-bold">NEO</h1>
       </WindowTitlebar>
 
       <div className="content justify-center text-center items-center">
--- src/spinner.tsx
+++ src/spinner.tsx
@@ -1,10 +1,8 @@
-import { Spinner } from "@fluentui/react-components"
-
 export default function ShowSpinner() {
   return (
     <div className="flex flex-col">
-      <Spinner appearance="inverted" size="extra-large" />
-      {/* <span className="text-lg">Welcome to AHQ Store</span> */}
+      <span className="loading loading-spinner loading-lg mx-auto"></span>
+      <span className="text-lg">Almost There</span>
     </div>
   );
 }
