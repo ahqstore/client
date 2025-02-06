ddc8bd0e41acb2ebcc496a81ec1214864dac1ff9
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-02-06 22:00:38 +0530

    fix++

--- src-plugin/src/models.rs
+++ src-plugin/src/models.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ShowCodeRequest {
-  pub value: Option<String>,
+  pub value: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
