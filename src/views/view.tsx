a3cb44c64869bcf520bc8452bd3ea66b8f1bfa9c
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-02-10 22:47:41 +0530

    Navigation Fix

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -26,7 +26,7 @@ import {
 } from "@fluentui/react-icons";
 
 import { platform } from "@tauri-apps/plugin-os";
-import { Library, LayoutGrid, Settings, User } from "lucide-react";
+import { Library, LayoutGrid, Settings, User, LibraryBig } from "lucide-react";
 
 import { useState } from "react";
 
@@ -36,21 +36,24 @@ const items = [
     id: 0,
     icon: <AppsRegular className="size-[1.5em]" />,
     iconFilled: <AppsFilled className="size-[1.5em]" />,
-    iconMobile: <LayoutGrid size="1.5em" />
+    iconMobile: <LayoutGrid size="1.5em" />,
+    iconMobileFilled: <LayoutGrid fill="currentcolor" size="1.5em" />,
   },
   {
     name: "Library",
     id: 1,
     icon: <AppsListRegular className="size-[1.5em]" />,
     iconFilled: <AppsListFilled className="size-[1.5em]" />,
-    iconMobile: <Library size="1.5em" />
+    iconMobile: <Library size="1.5em" />,
+    iconMobileFilled: <LibraryBig size="1.5em" />,
   },
   {
     name: "Profile",
     id: 2,
     icon: <PersonRegular className="size-[1.5em]" />,
     iconFilled: <PersonFilled className="size-[1.5em]" />,
-    iconMobile: <User size="1.5em" />
+    iconMobile: <User size="1.5em" />,
+    iconMobileFilled: <User fill="currentcolor" size="1.5em" />,
   },
   {
     name: "Settings",
@@ -58,6 +61,7 @@ const items = [
     icon: <SettingsRegular className="size-[1.5em]" />,
     iconFilled: <SettingsFilled className="size-[1.5em]" />,
     iconMobile: <Settings size="1.5em" />,
+    iconMobileFilled: <Settings className="rotate-12" size="1.5em" />,
   }
 ];
 
@@ -122,10 +126,10 @@ function BottomNavigation({ item, setItem }: { item: number, setItem: (_: number
   return <div className="dock dock-xl bg-neutral/30">
     {
       items.map((s) =>
-        <button key={`${s.id}`} className={s.id === item ? "dock-active" : ""} onClick={() => setItem(s.id)}>
+        <button key={`${s.id}`} className={s.id === item ? "dock-active transition-all" : "transition-all"} onClick={() => setItem(s.id)}>
           {
-            platform() != "android" ?
-              s.iconMobile
+            platform() == "android" ?
+              s.id === item ? s.iconMobileFilled : s.iconMobile
               :
               s.id === item ? s.iconFilled : s.icon
           }
