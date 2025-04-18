6bad28ed72e1c409c6920732cfe0972fd57f3683
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-04-18 23:07:28 +0530

    Update splash

--- src/views/apps/index.tsx
+++ src/views/apps/index.tsx
@@ -4,8 +4,6 @@ import { Search } from "lucide-react";
 export function AppsHome() {
   const splash = useSplash();
 
-  console.log(splash);
-
   return <>
     <div className="flex w-full justify-center text-center items-center">
       <div className="relative w-full max-w-xl">
