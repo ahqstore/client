6e98997bd936a35bfd81b8a4bef98766499f712b
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-05-07 22:08:10 +0530

    New Home Screen Layout + Animations

--- src/views/nav.tsx
+++ src/views/nav.tsx
@@ -9,7 +9,7 @@ function NavigationItem({ name, icon, i, iconFilled, id, item, setItem }: {
   item: number,
   setItem: (_: number) => void
 }) {
-  return <div className={`flex transition-all size-18 rounded-md cursor-pointer nav_item ${item == id ? "act_nav_item" : ""} ${i == 2 ? "mt-auto" : ""}`} onClick={() => setItem(id)}>
+  return <div className={`flex transition-all size-20 rounded-md cursor-pointer nav_item ${item == id ? "act_nav_item" : ""} ${i == 2 ? "mt-auto" : ""}`} onClick={() => setItem(id)}>
     <div className={`bg-[var(--win32-accent)] h-[40%] w-2 rounded-lg my-auto ${item == id ? "" : "hidden"}`}></div>
     <div className={`w-full h-full flex flex-col items-center justify-center text-center ${item == id ? "mr-2" : ""}`}>
       {item == id ? iconFilled : icon}
