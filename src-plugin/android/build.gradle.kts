bbc6da21abf4ccd06ee158dea01b5f9a09e2c444
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-07-20 22:53:49 +0530

    Downgrade core-ktx
    Signed-off-by: Akshanabha Chakraborty <ahqsecret@gmail.com>

--- src-plugin/android/build.gradle.kts
+++ src-plugin/android/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("ru.solrudev.ackpine:ackpine-core:$ackpineVersion")
     implementation("ru.solrudev.ackpine:ackpine-ktx:$ackpineVersion")
 
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.work:work-runtime-ktx:2.10.2")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
