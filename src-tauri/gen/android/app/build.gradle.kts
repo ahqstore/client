34efaf3717c5c48e7ce32b9b0526a2be65e476b7
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-08-17 13:46:00 +0530

    Add compose compiler

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("rust")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 val tauriProperties = Properties().apply {
