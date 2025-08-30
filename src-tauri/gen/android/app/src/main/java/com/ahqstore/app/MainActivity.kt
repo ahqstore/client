15bc1c8bee0169499f7f1a8e7325ba3ff172b1e9
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-08-30 10:00:24 +0530

    Update android

--- src-tauri/gen/android/app/src/main/java/com/ahqstore/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/ahqstore/app/MainActivity.kt
@@ -1,3 +1,11 @@
 package com.ahqstore.app
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
